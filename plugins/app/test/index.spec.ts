e8dd89932dfad3cfab5f0ae544b901e6ab9b641e
kagawagao <kingsongao1221@gmail.com>
2024-06-14 09:31:11 +0800

    test: fix app plugin test case

--- plugins/app/test/index.spec.ts
+++ plugins/app/test/index.spec.ts
@@ -5,25 +5,25 @@ import path from 'node:path'
 import OpenAPITransformAppPlugin from '../src'
 
 describe('app', () => {
-  // it('should generate app client with openapi v2', async () => {
-  //   await OpenAPIRunner.run([
-  //     {
-  //       url: path.resolve(__dirname, '../../../openapi_v2.json'),
-  //       namespace: 'artist',
-  //       plugins: [
-  //         new OpenAPITransformAppPlugin({
-  //           apiDir: path.resolve(__dirname, './fixtures/apis'),
-  //           serviceDir: path.resolve(__dirname, './fixtures/services'),
-  //           dtsDir: path.resolve(__dirname, './fixtures/interfaces'),
-  //         }),
-  //       ],
-  //     },
-  //   ])
-  //   const apiFileContent = await fs.readFile(path.resolve(__dirname, './fixtures/apis/artist.ts'), 'utf-8')
-  //   const serviceFileContent = await fs.readFile(path.resolve(__dirname, './fixtures/services/artist.ts'), 'utf-8')
-  //   expect(apiFileContent.trim()).not.toEqual('')
-  //   expect(serviceFileContent.trim()).not.toEqual('')
-  // })
+  it('should generate app client with openapi v2', async () => {
+    await OpenAPIRunner.run([
+      {
+        url: path.resolve(__dirname, '../../../openapi_v2.json'),
+        namespace: 'artist',
+        plugins: [
+          new OpenAPITransformAppPlugin({
+            apiDir: path.resolve(__dirname, './fixtures/apis'),
+            serviceDir: path.resolve(__dirname, './fixtures/services'),
+            dtsDir: path.resolve(__dirname, './fixtures/interfaces'),
+          }),
+        ],
+      },
+    ])
+    const apiFileContent = await fs.readFile(path.resolve(__dirname, './fixtures/apis/artist.ts'), 'utf-8')
+    const serviceFileContent = await fs.readFile(path.resolve(__dirname, './fixtures/services/artist.ts'), 'utf-8')
+    expect(apiFileContent.trim()).not.toEqual('')
+    expect(serviceFileContent.trim()).not.toEqual('')
+  })
 
   it('should generate app client with openapi v3', async () => {
     await OpenAPIRunner.run([
